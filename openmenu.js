d59bc12360aa5e5ea57bba9914f71e9b0d201b6f
iq0x <88105363+iq0x@users.noreply.github.com>
2021-07-31 13:19:12 +0200

    Create openmenu.js

---
+++ openmenu.js
@@ -0,0 +1,12 @@
+function openMenu() 
+{
+    var open = document.getElementById("menu");
+    if (open.style.display === "none") 
+    {
+        open.style.display = "block";
+    } 
+    else 
+    {
+        open.style.display = "none";
+    }
+} 
